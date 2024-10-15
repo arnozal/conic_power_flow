cba19ff9a98d3312e98c0377aa4add7bfd272264
arnozal <arnozal@us.es>
2024-10-15 10:39:10 +0200

    Structure of the code completed

--- code/lib.py
+++ code/lib.py
@@ -4,58 +4,55 @@ import numpy as np
 
 
 class grid:
-    def __init__(self, nodes, lines):
+    def __init__(self, nodes, lines, pros):
         self.nodes = self.add_nodes(nodes)                                      
-        self.lines = self.add_lines(lines, self.nodes)        
+        self.lines = self.add_lines(lines, self.nodes)  
+        self.pros = self.add_pros(pros, self.nodes)  
                 
     def add_nodes(self, nodes):
         nodes_list = list()
         for item in nodes:
-            nodes_list.append(node(item['id'], item['slack'], item['S'], item['fdp']))
+            nodes_list.append(node(item['id'], item['slack']))
         return nodes_list
         
     def add_lines(self, lines, nodes):
         lines_list = list()
         for item in lines:
-            lines_list.append(line(item['id'], item['From'], item['To'], item['R'], item['X'], item['Long'], nodes))
+            lines_list.append(line(item['id'], item['From'], item['To'], item['R'], item['X'], nodes))
         return lines_list
+        
+    def add_pros(self, pros, nodes):
+        pros_list = list()
+        for item in pros:
+            pros_list.append(prosumer(item['id'], item['Node'], item['P'], item['Q'], nodes))
+        return pros_list
 
 class node:
-    def __init__(self, ref, slack, S, fdp):
+    def __init__(self, ref, slack):
         self.ref = ref   
         self.slack = slack        
         self.lines = list()
-        
-    def check(self, currents = 0):
-        Ilines = 0
-        for line in self.lines:
-            if line.nodes[0] == self:
-                Ilines += line.I
-            else:
-                Ilines -= line.I
-        Iloads = - complex(self.P, - self.Q)/np.conjugate(self.U) + currents
-        return Ilines + Iloads 
+        self.pros = []
     
         
 class line:
-    def __init__(self, ref, From, To, R, X, long, nodes_list):
+    def __init__(self, ref, From, To, R, X, nodes_list):
         self.ref = ref     
-        self.Z = complex(R, X)*long  
+        self.Z = complex(R, X)
         self.G, self.B = np.real(1/self.Z), -np.imag(1/self.Z)
         self.Y = 1/self.Z
         self.nodes = [next((item for item in nodes_list if item.ref == From), None), 
                       next((item for item in nodes_list if item.ref == To), None)]   
         self.nodes[0].lines.append(self)
         self.nodes[1].lines.append(self)
-        
-    def check(self):
-        res = self.Z*self.I - (self.nodes[0].U - self.nodes[1].U)
-        res = self.I - self.Y*(self.nodes[0].U - self.nodes[1].U) 
-        return res
   
-        
-        
-        
+class prosumer:
+    def __init__(self, ref, node_id, P, Q, nodes_list):
+        self.ref = ref
+        self.P = P
+        self.Q = Q        
+        self.node = next((item for item in nodes_list if item.ref == node_id), None)
+        self.node.pros.append(self)
         
         
         
--- code/main.py
+++ code/main.py
@@ -2,31 +2,35 @@
 import numpy as np
 import lib
 
-# Network configuration
-#     0 ---- 1 ----- 2 
-#             \     / \
-#              \   /   \
-#               \ /     \
-#                3 ----- 4 
-
-Sbase = 100e3
+Sbase = 1e6
 Ubase = 20e3
 Zbase = (Ubase**2)/Sbase
 
-# Buses 
+# Nodes
 Nodes = [{'id': 0, 'slack': True  },
          {'id': 1, 'slack': False },
          {'id': 2, 'slack': False },
-         {'id': 3, 'slack': False },
-         {'id': 4, 'slack': False }]
+         {'id': 3, 'slack': False }]
 
 # Lines
-Lines = [{'id': 0,  'From': 0,  'To': 1,  'R': 0.2/Zbase, 'X': 0.2/Zbase, 'Long': 1 }, 
-         {'id': 1,  'From': 1,  'To': 2,  'R': 0.2/Zbase, 'X': 0.2/Zbase, 'Long': 1.1 },
-         {'id': 2,  'From': 1,  'To': 3,  'R': 0.2/Zbase, 'X': 0.2/Zbase, 'Long': 1 },
-         {'id': 3,  'From': 2,  'To': 3,  'R': 0.2/Zbase, 'X': 0.2/Zbase, 'Long': 1 },
-         {'id': 4,  'From': 2,  'To': 4,  'R': 0.2/Zbase, 'X': 0.2/Zbase, 'Long': 1.1 },
-         {'id': 5,  'From': 3,  'To': 4,  'R': 0.2/Zbase, 'X': 0.2/Zbase, 'Long': 1 }]
+Lines = [{'id': 0,  'From': 0,  'To': 1,  'R': 0.161*4/Zbase, 'X': 0.190*4/Zbase }, 
+         {'id': 1,  'From': 1,  'To': 2,  'R': 0.161*2/Zbase, 'X': 0.190*2/Zbase }, 
+         {'id': 2,  'From': 2,  'To': 3,  'R': 0.161*5/Zbase, 'X': 0.190*5/Zbase }]
+
+# Prosumers
+Pros = [{'id': 0, 'Node': 1, 'P': -2e6/Sbase, 'Q': -1.5e6/Sbase},
+        {'id': 1, 'Node': 2, 'P': -1.6e6/Sbase, 'Q': -1.2e6/Sbase},
+        {'id': 2, 'Node': 3, 'P': -6.4e6/Sbase, 'Q': -2.4e6/Sbase},]
 
 # Constructing network and solving power flow
-net = lib.grid(Nodes, Lines)
+net = lib.grid(Nodes, Lines, Pros)
+
+
+# Algunos comandos interesantes
+# net.__dict__
+# net.nodes[0].__dict__
+# net.nodes[0].lines[0].__dict__
+# net.nodes[0].lines[0].nodes[0].__dict__
+
+# for node in net.nodes:
+#     print(node.__dict__)
