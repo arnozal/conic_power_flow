c399fb9fcd346630496244393f270a7749c7a21f
OscarLopezLopez <oscarloplop00@gmail.com>
2024-10-28 13:48:09 +0100

    Add files via upload

--- code/lib.py
+++ code/lib.py
@@ -1,14 +1,17 @@
 # Importing required libraries
 import numpy as np
-import cmath
-
+from scipy.optimize import minimize, NonlinearConstraint, LinearConstraint
 
 class grid:
     def __init__(self, nodes, lines, pros):
         self.nodes = self.add_nodes(nodes)                                      
         self.lines = self.add_lines(lines, self.nodes)  
         self.pros = self.add_pros(pros, self.nodes)  
-                
+        self.n = len(self.nodes)
+        self.m = len(self.lines)
+        self.x_size = self.n+(2*self.m)-1
+        self.obtain_index()
+        
     def add_nodes(self, nodes):
         nodes_list = list()
         for item in nodes:
@@ -27,6 +30,101 @@ class grid:
             pros_list.append(prosumer(item['id'], item['Node'], item['P'], item['Q'], nodes))
         return pros_list
     
+    def obtain_index(self):
+        n_aux = 0
+        matrizX = np.zeros((self.x_size,1), dtype=float)
+        while n_aux < self.n:
+            self.nodes[n_aux].index = n_aux -1
+            n_aux += 1
+        
+        n_aux2 = 0    
+        while n_aux2 < self.m:   
+            self.lines[n_aux2].index.append(n_aux - 1)
+            n_aux += 1
+            n_aux2 += 1
+        
+        n_aux3 = 0    
+        while n_aux3 < self.m:   
+            self.lines[n_aux3].index.append(n_aux - 1)
+            n_aux += 1
+            n_aux3 += 1   
+    
+        # matrizX1 = np.array([node.index for node in self.nodes]).reshape(self.n, 1)
+        # matrizX2 = np.array([line.index[0] for line in self.lines]).reshape(self.m, 1)           
+        # matrizX3 = np.array([line.index[1] for line in self.lines]).reshape(self.m, 1)     
+        # matrizX = np.vstack((matrizX1, matrizX2, matrizX3)) 
+        
+        # self.X = matrizX
+        self.X = np.zeros(self.x_size)
+        self.X[:self.n - 1] = 1
+        
+    def obtain_A(self):
+        matrizA = np.zeros(((2*self.n)-2, (self.n+2*self.m)-1), dtype=float)
+        
+        n_aux = 0
+        for i, node in enumerate(self.nodes[1:]):
+            matrizA[2*i, n_aux] = np.sum([line.G for line in node.lines])
+            matrizA[2*i+1, n_aux] = np.sum([line.B for line in node.lines])
+            n_aux += 1
+            
+            for j, line in enumerate(node.lines):
+                if node == line.nodes[0]:
+                    matrizA[2*i, line.index[0]] = -line.G
+                    matrizA[2*i, line.index[1]] = -line.B
+                    matrizA[2*i+1, line.index[0]] = -line.B
+                    matrizA[2*i+1, line.index[1]] = line.G
+                else:
+                    matrizA[2*i, line.index[0]] = -line.G
+                    matrizA[2*i, line.index[1]] = line.B
+                    matrizA[2*i+1, line.index[0]] = -line.B
+                    matrizA[2*i+1, line.index[1]] = -line.G            
+        self.A = matrizA
+        
+    def ineq(self, X):
+        rest = []
+        for line in self.lines:
+            rest.append(line.ineq(X))
+            
+        return rest
+
+    def solve_pf(self):
+        self.obtain_A()
+        self.obtain_B()
+        self.obtain_f()
+        
+        lc = LinearConstraint(self.A, self.B, self.B)
+        nlc = NonlinearConstraint(self.ineq, -np.inf, 0)
+        fo = lambda x: self.f.dot(x)
+        sol = minimize(fo, self.X, constraints=(lc, nlc))
+        return sol
+        
+    
+    def obtain_B(self):
+        matrizB = np.zeros(2*self.n-2, dtype=float)
+        
+        for i, node in enumerate(self.nodes[1:]):
+           
+            for x in node.pros:
+                matrizB[2*i] += x.P
+                matrizB[2*i+1] += x.Q             
+        self.B = matrizB
+        
+    
+    def obtain_f(self):
+        f = np.zeros((1, self.x_size))
+        # aux = self.n
+        
+        # while aux < (self.m + self.n):
+        #     f[0, aux] = 1
+        #     aux += 1
+        
+        f[0, self.n - 1:(self.n+self.m) - 1] = -1
+        self.f = f
+        # cuenta = np.dot(f, X)
+        # self.cuenta = cuenta
+           
+
+        
     def pf(self):
         n = len(self.nodes)
         m = len(self.lines)
@@ -43,8 +141,8 @@ class grid:
         #MATRIZ B
         for i, node in enumerate(self.nodes):
             for x in node.pros:
-                matrizB[2*i] = x.P
-                matrizB[(2*i)+1] = x.Q
+                matrizB[2*i] += x.P
+                matrizB[(2*i)+1] += x.Q
         
         
         #MATRIZ X
@@ -171,11 +269,12 @@ class grid:
         print("La matriz A es:\n")
         print(matrizA)
         print("")
-        print("Al final, el resultado de A*x = b es: \n")
-        print(f"{matrizA} * {matrizX} = {matrizB}")
+        
+        return matrizA,matrizB
+    
     def cuenta_1 (self, lineas):
         Suma1 = sum(linea.G  for linea in lineas)
-        Suma2 = sum(linea.G  for linea in lineas)
+        Suma2 = sum(linea.B  for linea in lineas)
         return Suma1, Suma2
     def cuenta_2 (self, lineas):
         G_anterior = lineas[0].G
@@ -197,7 +296,9 @@ class node:
         self.slack = slack        
         self.lines = list()
         self.pros = []
-        self.Ckk = []
+        self.Ckk = None
+        self.Ctt = None
+        self.index = None
         
 class line:
     def __init__(self, ref, From, To, R, X, nodes_list):
@@ -209,9 +310,21 @@ class line:
                       next((item for item in nodes_list if item.ref == To), None)]   
         self.nodes[0].lines.append(self)
         self.nodes[1].lines.append(self)
-        self.Ckt = []
-        self.Skt = []
-    
+        self.Ckt = None
+        self.Skt = None
+        self.index = []  
+        
+    def ineq(self, X):
+        if self.nodes[0].slack == True:
+            Ckk = 1
+        else:
+            Ckk = X[self.nodes[0].index]
+        Ctt = X[self.nodes[1].index]
+        self.Ckt = X[self.index[0]]
+        self.Skt = X[self.index[1]]
+        ineq = self.Ckt**2 + self.Skt**2 - Ckk * Ctt
+        return ineq
+        
 class prosumer:
     def __init__(self, ref, node_id, P, Q, nodes_list):
         self.ref = ref
