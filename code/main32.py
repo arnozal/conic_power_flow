760abb451c20c869ba41fbefb479d25206d42782
Óscar López <oscarloplop00@gmail.com>
2024-10-31 12:51:51 +0100

    TFG red 32 nodos

--- code/main32.py
+++ code/main32.py
@@ -1,18 +1,14 @@
 import numpy as np
 import lib
 
-Sbase = 100e3
+Sbase = 0.1e6
 Ubase = 12.66e3
 Zbase = (Ubase**2)/Sbase
 
 Nodes = []
 Lines = []
-Nodes.append({
-              'id': 0,
-              'S': 0,
-              'fdp': 0,
-              'slack': True
-              })
+Pros = []
+Nodes.append({'id': 0, 'S': 0, 'fdp': 0, 'slack': True})
 
 flag = False
 data = 'something'
@@ -26,11 +22,11 @@ with open('red32nudos/datos_32.m') as fp:
                 break
             Lines.append({
                           'id': int(data[0])-1, 
-                          'From': int(data[1]), 
+                          'From': int(data[1]),
                           'To': int(data[2]),
                           'R': float(data[3])/Zbase,
                           'X': float(data[4])/Zbase,
-                          'Long':1.0,
+                          'Long':1.0
                           })     
             if int(data[2]) not in [node['id'] for node in Nodes]:
                 Nodes.append({
@@ -38,7 +34,15 @@ with open('red32nudos/datos_32.m') as fp:
                               'S': np.sqrt((float(data[5])*1e3)**2 + (float(data[6])*1e3)**2)/Sbase,
                               'fdp': float(data[5])/(np.sqrt(float(data[5])**2 + float(data[6])**2)) if float(data[5]) != 0 else 0,
                               'slack': False
-                              })   
+                              })
+            if float(data[5]) != 0 and float(data[6]) != 0:
+                Pros.append({
+                                  'id': int(data[2])-1, 
+                                  'Node': int(data[2]), 
+                                  'P': float(data[5])*1000/Sbase, 
+                                  'Q': float(data[6])*1000/Sbase
+                                  })
+    
         if 'datsis' in data:
             flag = True
             data = data.split('datsis = [')[1]
@@ -58,4 +62,16 @@ with open('red32nudos/datos_32.m') as fp:
                           'fdp': float(data[5])/(np.sqrt(float(data[5])**2 + float(data[6])**2)),
                           'slack': False
                           })
+            Pros.append({
+                             'id': int(data[2])-1, 
+                             'Node': int(data[2]), 
+                             'P': float(data[5])/Sbase, 
+                             'Q': float(data[6])/Sbase
+                             })
         data = fp.readline()
+
+net = lib.grid(Nodes, Lines, Pros)
+
+sol = net.solve_pf()
+Volt = net.obtain_volt()
+I = net.intensity()
